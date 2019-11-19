ed9ef287dc959dd223c2e2a87ae79de553ee97a1
Jenniekibiri <josphatje@gmail.com>
2019-11-19 14:16:25 +0300

    createnewgifendpoint

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-var express = require('express');
-var app = express();
-var exports = module.exports = {};
+const express = require('express')
+const controller = require('./Controllers/controller')
+const Auth = require('./Controllers/auth')
+const fileUpload =require('express-fileupload')
 
-app.get('/', function(req, res){
-  res.send('Hello World');
+const app = express()
+app.use(fileUpload({useTempFiles:true}))
+
+
+app.get('/feed', Auth.verifyToken, controller.getFeed);
+
+app.get('/article/:article_id', Auth.verifyToken, controller.getArticleById);
+
+app.post('/auth/create-user/v1', Auth.verifyToken, controller.createNewUser);
+
+app.post('/auth/sign-in/v1', controller.Authenticate)
+
+app.post('/gifs', Auth.verifyToken, controller.createNewGif)
+
+app.listen(process.env.PORT || 5000,() => {
+  console.log("listening on port  " )
 });
 
-var server = app.listen(3000, function(){
-  console.log('Magic is happening on port  3000 watch out');
-});
-
-exports.closeServer = function(){
-  server.close();
-};
