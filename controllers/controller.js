2c358f59b77e42e91624df5ba2a4ef3b3b19f416
Jenniekibiri <josphatje@gmail.com>
2019-11-19 12:40:59 +0300

    commiting to getfeed branch

---
+++ controllers/controller.js
@@ -0,0 +1,46 @@
+//const pg =require('pg');
+const { Client } = require('pg');
+//const crypto = require('bcryptjs')
+//var cloudinary = require('cloudinary').v2;
+ //const fileUpload =require('express-fileupload')
+//const jwt = require('jsonwebtoken');
+
+
+/*cloudinary.config({ 
+    cloud_name: 'teamworkapp', 
+    api_key: '185312112164313', 
+    api_secret: '8EpE_sQO-Z8sQaYywuLlKoSh9zQ' 
+  });
+  */
+
+var connectionString = "postgres://postgres:5463jeny@localhost:5432/teamwork";
+
+const client = new Client({
+    connectionString: connectionString
+});
+
+//Methode to validate email
+// function isValidEmail(email){
+//     return /\S+@\S+\.\S+/.test(email);
+// }
+/*
+
+function generateToken(id){
+    const token = jwt.sign({userId: id}, 'secret', {expiresIn: '30d'})
+    return token;
+}
+*/
+client.connect(err => console.log(err));
+
+exports.getFeed = (req , res) => {
+    //Controllers to query all data from db and send for home feed displays
+    
+    client.query('SELECT * FROM articles, gifs', (err, result) => {
+         res.send(result.rows)
+         console.log(typeof result.rows)
+    })
+    
+    
+}
+
+module.exports = exports; 
--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-var express = require('express');
-var app = express();
-var exports = module.exports = {};
+const express = require('express')
+const controller = require('./Controllers/controller')
+const Auth = require('./Controllers/auth')
+const fileUpload =require('express-fileupload')
 
-app.get('/', function(req, res){
-  res.send('Hello World');
+const app = express()
+app.use(fileUpload({useTempFiles:true}))
+
+
+app.get('/feed', Auth.verifyToken, controller.getFeed);
+app.listen(process.env.PORT || 5000,() => {
+  console.log("listening on port  " )
 });
-
-var server = app.listen(3000, function(){
-  console.log('Magic is happening on port  3000 watch out');
-});
-
-exports.closeServer = function(){
-  server.close();
-};
