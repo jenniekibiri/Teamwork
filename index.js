d3c0e1f58f8e29415691ba72a6ecf2cec7e0cf98
Jenniekibiri <josphatje@gmail.com>
2019-11-19 14:44:43 +0300

    created commenton article endpoint

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-var express = require('express');
-var app = express();
-var exports = module.exports = {};
+const express = require('express')
+const controller = require('./Controllers/controller.')
+const Auth = require('./Controllers/auth')
+const fileUpload =require('express-fileupload')
+//var cloudinary = require('cloudinary');
+const app = express()
+app.use(fileUpload({useTempFiles:true}))
 
-app.get('/', function(req, res){
-  res.send('Hello World');
+app.get('/feed', Auth.verifyToken, controller.getFeed)
+
+app.get('/article/:article_id', Auth.verifyToken, controller.getArticleById)
+
+app.post('/auth/create-user/v1', Auth.verifyToken, controller.createNewUser)
+
+app.post('/auth/sign-in/v1', controller.Authenticate)
+
+app.post('/gifs', Auth.verifyToken, controller.createNewGif)
+
+app.post('/article', Auth.verifyToken, controller.createArticle)
+
+app.post('/articles/article_id/comments', Auth.verifyToken, controller.CommentArticle)
+
+/*app.post('/gifs/gif_id/comments/v1', Auth.verifyToken, controller.commentGif)
+
+app.put('/article/edit_article', Auth.verifyToken, controller.editArticle)
+
+app.delete('/article/:article_id', controller.deleteArticle)
+
+app.delete('/gifs/:gif_id', controller.deleteGif)
+*/
+var server = app.listen(process.env.PORT || 5000,() => {
+    console.log("listening on port 5000" )
 });
-
-var server = app.listen(3000, function(){
-  console.log('Magic is happening on port  3000 watch out');
-});
-
-exports.closeServer = function(){
-  server.close();
-};
