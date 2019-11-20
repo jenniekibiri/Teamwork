d6377d2d67edcd9b271428ef87c1caca462ada50
Jenniekibiri <josphatje@gmail.com>
2019-11-21 02:21:39 +0300

    modified get feed endpoint

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var express = require('express');
+const controller = require('./Controllers/controller')
+const Auth = require('./Controllers/auth')
 var app = express();
 var exports = module.exports = {};
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.send('get feed');
 });
+
+app.get('/feed', Auth.verifyToken, controller.getFeed);
+
 
 var server = app.listen(3000, function(){
   console.log('Magic is happening on port  3000 watch out');
--- spec/getfeed.test.js
+++ spec/getfeed.test.js
@@ -1,8 +1,9 @@
 var request = require("request");
 var helloWorld = require("../index.js")
 var base_url = "http://localhost:3000/"
+ var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjU1LCJpYXQiOjE1NzM5ODg5NDIsImV4cCI6MTU3NjU4MDk0Mn0.wQrKgnxKgx8Y_LwrSJcpRJUgKKiRRv5rRkm3t59wtxw'
 
-describe("Hello World Server", function() {
+describe(" get feed Server", function() {
   describe("GET /", function() {
     it("returns status code 200", function(done) {
       request.get(base_url, function(error, response, body) {
@@ -11,12 +12,39 @@ describe("Hello World Server", function() {
       });
     });
 
-    it("returns Hello World", function(done) {
+    it("returns get feed", function(done) {
       request.get(base_url, function(error, response, body) {
-        expect(body).toBe("Hello World");
-        helloWorld.closeServer();
+        expect(body).toBe("get feed");
         done();
+        
       });
     });
-  });
-});
+  },5000);
+ describe("GET /feed",()=>{
+   var data = { 
+    url: 'http://localhost:3000/feed',
+    headers:{
+      'x-access-token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjU1LCJpYXQiOjE1NzM5ODg5NDIsImV4cCI6MTU3NjU4MDk0Mn0.wQrKgnxKgx8Y_LwrSJcpRJUgKKiRRv5rRkm3t59wtxw'
+    }
+   }
+   it("returns all data from gifs and articles",()=>{
+     request.get(data,(error,response,body)=>{
+
+      expect(()=>{
+        JSON.parse(body)
+      })
+       helloWorld.closeServer();
+        done();
+     })
+   })
+// it('api should have list of all gif and articles',()=>{
+//   request.get(data,(error,response,body)=>{
+//     let feed  = JSON.parse(body);
+//     const articleRows =articles.map((articleRows))
+
+    
+//   })
+// })
+
+ },5000);
+ })
