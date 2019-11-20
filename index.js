15a9e41cd7ebb341ff57bae0815a06d3c5f5d20d
Jenniekibiri <josphatje@gmail.com>
2019-11-20 16:21:44 +0300

    modified getfeed endpoint

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const express = require('express');
-const controller = require('./Controllers/controller');
-const Auth = require('./Controllers/auth');
-var request = require("request");
+var express = require('express');
+var app = express();
+var exports = module.exports = {};
 
-
-
-const app = express();
-
-app.get('/', (request,response)=>{
-  response.status(200).send('jennie is a developer');
+app.get('/', function(req, res){
+  res.send('Hello World');
 });
 
-/*app.get('/feed', Auth.verifyToken, controller.getFeed);*/
- var server = app.listen(process.env.PORT || 3000,() => {
-  console.log("listening on port 3000" )
+var server = app.listen(3000, function(){
+  console.log('Magic is happening on port  3000 watch out');
 });
 
-module.exports = server;
+exports.closeServer = function(){
+  server.close();
+};
--- spec/getfeed.test.js
+++ spec/getfeed.test.js
@@ -1,64 +1,22 @@
-/* eslint-disable no-undef */
 var request = require("request");
+var helloWorld = require("../index.js")
+var base_url = "http://localhost:3000/"
 
-describe("Server",()=>{
-   let server;
-  beforeAll(()=>{
-    server = require("../index");
-  });
-  afterAll(()=>{
-    server.close();
-  });
-describe("GET /",()=>{
-  var data ={};
-  beforeAll((done)=>{
-    request.get('http://localhost:3000/',(error,response,body)=>{
-      data.status =response.statusCode;
-      data.body=body;
-      done();
+describe("Hello World Server", function() {
+  describe("GET /", function() {
+    it("returns status code 200", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    it("returns Hello World", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(body).toBe("Hello World");
+        helloWorld.closeServer();
+        done();
+      });
     });
   });
-  it('Status 200',()=>{
-expect(data.status).toBe(200);
-  });
-
-  it("body",()=>{
-    expect(data.body).toBe("jennie is a developer");
-
-  server.close();
-    
-  });
- 
 });
-});
-/*
-var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTU3Mzk5MTg0MSwiZXhwIjoxNTc2NTgzODQxfQ.T0dC8yn8y1R-cBmmHtm_GoqXUs0c6gFQoaCFdl1pXsA'
-
-describe("Server", function() {
- 
-  describe("GET /feed", () => {
-    var options = {
-      url: 'http://localhost:5000/feed',
-      headers:{
-          'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTU3Mzk5MTg0MSwiZXhwIjoxNTc2NTgzODQxfQ.T0dC8yn8y1R-cBmmHtm_GoqXUs0c6gFQoaCFdl1pXsA'
-      }
-  }
-      it("returns all the data of articles and gifs", (done) => {
-          request.get(options, (error, response, body) => {
-              let body2 = JSON.parse(body)
-              
-              expect(typeof body2).toBe('object')
-              if(error){
-                  return error;
-              }
-              helloWorld.closeServer();
-              done();
-          })
-      })
-
-    
- })
-
- })
-
-*/
