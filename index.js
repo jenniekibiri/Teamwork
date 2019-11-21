220951ca3fbe0d86290d12857f1901c195650f7f
Jenniekibiri <josphatje@gmail.com>
2019-11-21 03:26:01 +0300

    modified getbyarticleid route

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const express = require('express')
-const controller = require('./Controllers/controller')
-const Auth = require('./Controllers/auth')
-const fileUpload =require('express-fileupload')
+var express = require('express');
+const controller = require('./controllers/controller.js')
+const Auth = require('./controllers/auth.js')
+var app = express();
+var exports = module.exports = {};
 
-const app = express()
-app.use(fileUpload({useTempFiles:true}))
-
-
-app.get('/feed', Auth.verifyToken, controller.getFeed);
-
-app.get('/article/:article_id', Auth.verifyToken, controller.getArticleById)
-
-app.listen(process.env.PORT || 5000,() => {
-  console.log("listening on port  " )
+app.get('/', function(req, res){
+  res.send('get feed');
 });
 
+app.get('/feed', Auth.verifyToken, controller.getFeed);
+app.get('/article/:article_id', Auth.verifyToken, controller.getArticleById)
+
+
+var server = app.listen(3000, function(){
+  console.log('Magic is happening on port  3000 watch out');
+});
+
+exports.closeServer = function(){
+  server.close();
+};
